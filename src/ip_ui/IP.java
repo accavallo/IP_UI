4b1f1e71f7acefc125cb5bc51fbb1e073e667e3c
Tony <Tony@Tony-PC>
2018-03-28 20:59:25 -0500

    Initial commit

---
+++ src/ip_ui/IP.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ip_ui;
+
+/**
+ *
+ * @author Tony
+ */
+public class IP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IP_UI ui = new IP_UI();
+        ui.setVisible(true);
+        ui.setLocationRelativeTo(null);
+    }
+    
+}
